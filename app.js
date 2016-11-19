09822b234a09c6af933498e8b3b584313e9c812f
Xien Lee <kylesentient@gmail.com>
2016-11-19 07:29:37 +0100

    Second commit

--- app.js
+++ app.js
@@ -67,6 +67,6 @@ app.post('/campgrounds', function (req, res) {
 
 // ====================================== Server ======================================
 
-app.listen(3000, function () {
-    console.log('Server started on port 3000');
+app.listen(8080, '0.0.0.0', function () {
+    console.log('Server started on port 8080');
 })
